eb92b04c1e5cf63537c9e3e637b95d0b6844661f
srkeys <srkeys2018@gmail.com>
2022-03-23 06:52:19 +0300

    2 рабочие вкладки

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 var main = function () {
     "use strict";
-    var tabNumber;
-    for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
-        var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-        $(tabSelector).on("click", function () {
-            $(".tabs span").removeClass("active");
-            $(this).addClass("active");
-            return false;
+
+    var toDos = [
+        "Закончить писать эту книгу",
+        "Вывести Грейси на прогулку в парк",
+        "Ответить на электронные письма",
+        "Подготовиться к лекции в понедельник",
+        "Обновить несколько новых задач",
+        "Купить продукты"
+        ];        
+
+    $(".tabs a span").toArray().forEach(function (element) {
+    // создаем обработку щелчков для этого элемента
+    $(element).on("click", function () {
+        // поскольку мы используем версию элемента jQuery,
+        // нужно создать временную переменную,
+        // чтобы избежать постоянного обновления
+        var $element = $(element), $content;
+        $(".tabs a span").removeClass("active");
+        $element.addClass("active");
+        $("main .content").empty();
+        $content = $("<ul>");
+        if ($element.parent().is(":nth-child(1)")) {
+            for (var i = toDos.length - 1; i > -1; i--) {
+                $content.append($("<li>").text(toDos[i]));
+                }
+
+        } else if ($element.parent().is(":nth-child(2)")) {
+            toDos.forEach(function (todo) {
+                $content.append($("<li>").text(todo));
             });
-        }
-    };
-    
-    
+            
+        } else if ($element.parent().is(":nth-child(3)")) {
+            console.log("Щелчок на третьей вкладке!");
+            }
+        $("main .content").append($content);
+        return false;
+        });
+    });
+    $(".tabs a:first-child span").trigger("click");
+};
+
 $("document").ready(main);
