3cac6abe6469093c56f41b6c1176aef4f8200476
srkeys <srkeys2018@gmail.com>
2022-03-29 23:43:44 +0300

    создание пользовательского интерфейса

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ var main = function (toDoObjects) {
 
     $(".tabs a span").toArray().forEach(function (element) {
     $(element).on("click", function () {
-        var $element = $(element),
-        $content;
+        var $element = $(element), $content;
         $(".tabs a span").removeClass("active");
         $element.addClass("active");
         $("main .content").empty();
@@ -24,8 +23,47 @@ var main = function (toDoObjects) {
             toDos.forEach(function (todo) {
                 $content.append($("<li>").text(todo));
             });
-            
         } else if ($element.parent().is(":nth-child(3)")) {
+            // ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ
+            console.log("щелчок на вкладке Теги");
+            var organizedByTag = [
+                {
+                "name": "покупки",
+                "toDos": ["Купить продукты"]
+                },
+                {
+                "name": "рутина",
+                "toDos": ["Купить продукты", "Вывести Грейси на прогулку в парк"]
+                },
+                {
+                "name": "писательство",
+                "toDos": ["Сделать несколько новых задач", "Закончить писать книгу"]
+                },
+                {
+                "name": "работа",
+                "toDos": ["Сделать несколько новых задач", "Подготовиться к лекции в понедельник",
+                "Ответить на электронные письма", "Закончить писать книгу"]
+                },
+                {
+                "name": " преподавание",
+                "toDos": ["Подготовиться к лекции в понедельник"]
+                },
+                {
+                "name": "питомцы",
+                "toDos": ["Вывести Грейси на прогулку в парк "]
+                }
+                ];
+                organizedByTag.forEach(function (tag) {
+                    var $tagName = $("<h3>").text(tag.name),
+                    $content = $("<ul>");
+                    tag.toDos.forEach(function (description) {
+                    var $li = $("<li>").text(description);
+                    $content.append($li);
+                    });
+                    $("main .content").append($tagName);
+                    $("main .content").append($content);
+                    });                                              
+        } else if ($element.parent().is(":nth-child(4)")) {
             $(".content").append(
 				'<input type="text" class="inp">'+
 				'<button class="btn">Добавить</button>'
