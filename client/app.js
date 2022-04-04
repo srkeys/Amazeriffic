8a1e85fe3f7afb4a8df3ceb0c41b6a7422b1ef1a
srkeys <srkeys2018@gmail.com>
2022-04-04 03:15:11 +0300

    добавление взаимодействия с клиентом

--- client/app.js
+++ client/app.js
@@ -23,20 +23,6 @@ var organizeByTags = function (toDoObjects) {
 return result;
 }
 
-// var clearflicker = function() {
-//     $("main .photos").empty();
-//     var but_abort = document.getElementById("search");
-//     but_abort.addEventListener("click", function(event) {
-//         console.log("Действие было отменено");
-//         event.preventDefault();
-//     }, false)
-//     //$("body .script_photos").empty();
-//     // $(document).trigger(function () {
-//     //     // вызов функции main с аргументом в виде объекта toDoObjects
-//     //     slideshow("");
-//     // });
-// }
-
 var updateToDos = function(toDoObjects) {
     var todos = toDoObjects.map(function (toDo) {
         // просто возвращаем описание
@@ -50,11 +36,6 @@ var main = function (toDoObjects) {
     "use strict";
 
     var toDos = updateToDos(toDoObjects);
-    // var toDos = toDoObjects.map(function (toDo) {
-    //     // просто возвращаем описание
-    //     // этой задачи
-    //     return toDo.description;
-    // });
 
     $(".tabs a span").toArray().forEach(function (element) {
     $(element).on("click", function () {
@@ -73,9 +54,7 @@ var main = function (toDoObjects) {
                 $content.append($("<li>").text(todo));
             });
         } else if ($element.parent().is(":nth-child(3)")) {
-            // ЭТО КОД ДЛЯ ВКЛАДКИ ТЕГИ
             console.log("щелчок на вкладке Теги");
-            //var organizedByTag = organizeByTag(toDoObjects);
             var tags = organizeByTags(toDoObjects);
                 tags.forEach(function (tag) {
                     var $tagName = $("<h3>").text(tag.name),
@@ -96,31 +75,26 @@ var main = function (toDoObjects) {
                 $button.on("click", function () {
                     var description = $input.val(),
                     // разделение в соответствии с запятыми
-                    tags = $tagInput.val().replace(/\s+/g, '').split(",");
-                    toDoObjects.push({"description" : description, "tags" : tags });
-                    alert('Новое задание "' + description + '" успешно добавлено! С тегами "' + tags + '" ');
+                    tags = $tagInput.val().replace(/\s+/g, '').split(","),
+                    newToDo = {"description" : description, "tags" : tags };
+                    // toDoObjects.push({"description" : description, "tags" : tags });
+                    // alert('Новое задание "' + description + '" успешно добавлено! С тегами "' + tags + '" ');
                     // здесь мы отправляем быстрое сообщение на маршрут списка задач
-                    $.post("todos", {toDoObjects}, function (response) {
+                    $.post("todos", newToDo, function (result) {
                         // этот обратный вызов выполняется при ответе сервера
-                        console.log("Мы отправили данные и получили ответ сервера!");
-                        console.log(response);
+                        // console.log("Мы отправили данные и получили ответ сервера!");
+                        console.log(result);
+                        alert(result);
+                        // обновление toDos
+                        toDos = updateToDos(toDoObjects);
+                        $input.val("");
+                        $tagInput.val("");
                     });
-                    // обновление toDos
-                    toDos = updateToDos(toDoObjects);
-                    $input.val("");
-                    $tagInput.val("");
                 });
                 $content = $("<div>");
                 $content.append($("<div class= \"div_descr\">").append($inputLabel).append($input))
                 $content.append($("<div class= \"div_but\">").append($button))
                 $content.append($("<div class= \"div_tags\">").append($tagLabel).append($tagInput))
-            // } else if ($element.parent().is(":nth-child(5)")) { 
-			// 	var js = document.createElement('script');
-			// 	js.src = "test_2.js";
-			// 	document.body.appendChild(js);
-            //     // $("body .script_photos").empty();
-            //     // var js = $("<script>").attr("src", "test_2.js")
-            //     // $("body .script_photos").append(js);
 			}
         $("main .content").append($content);
         return false;
