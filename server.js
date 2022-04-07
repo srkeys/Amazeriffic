62d465f43b68eb09efb1662ae08fb5abc6f11d2b
srkeys <srkeys2018@gmail.com>
2022-04-07 21:36:37 +0300

    удаление лишних комментариев

--- server.js
+++ server.js
@@ -3,44 +3,9 @@ http = require("http"),
 app = express(),
 // импортируем библиотеку mongoose
 mongoose = require("mongoose");
-// toDos = [
-//     {        
-//         "description" : "Купить продукты",
-//         "tags" : [ "шопинг", "рутина" ]
-//         },
-//         {
-//         "description" : "Сделать несколько новых задач",
-//         "tags" : [ "писательство", "работа" ]
-//         },
-//         {
-//         "description" : "Подготовиться к лекции в понедельник",
-//         "tags" : [ "работа", "преподавание" ]
-//         },
-//         {
-//         "description" : "Ответить на электронные письма",
-//         "tags" : [ "работа" ]
-//         },
-//         {
-//         "description" : "Вывести Грейси на прогулку в парк",
-//         "tags" : [ "рутина", "питомцы" ]
-//         },
-//         {
-//         "description" : "Закончить писать книгу",
-//         "tags" : [ "писательство", "работа" ]
-//     }       
-// ];
 app.use(express.static(__dirname + "/client"));
 app.use(express.urlencoded());
 mongoose.connect('mongodb://localhost:27017');
-// , {
-// 		useNewUrlParser: true,
-// 		useCreateIndex: true,
-// 		useUnifiedTopology: true 
-// 	}).then(res => {
-// 		console.log("DB Connected!")
-// 	}).catch(err => {
-// 		console.log(Error, err.message);
-// 	});
 
 // Это модель Mongoose для задач
 var ToDoSchema = mongoose.Schema({
@@ -82,17 +47,3 @@ app.post("/todos", function (req, res) {
         }
     });
 });
-
-
-// app.post("/todos", function (req, res) {
-//     // сейчас объект сохраняется в req.body
-//     var newToDo = req.body;
-//     console.log(newToDo);
-//     toDos.push(newToDo);
-//     // отправляем простой объект
-//     res.json({"message":"Вы размещаетесь на сервере!"});
-// });
-// //  http.createServer(app).listen(3000);
-// app.get("/todos.json", function (req, res) {
-//     res.json(toDos);    
-// });
